4cfc1264ef47116f71ebb40dd8b6908fdb765bb1
DanielJanowicz <102616987+DanielJanowicz@users.noreply.github.com>
2022-11-16 16:25:18 -0500

    Working Commit

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import streamlit as st
 
 # Loading dataset
 registry_df = pd.read_csv('follow_up.csv')
+vehicle_df = pd.read_csv('vehicle_accident_data.csv')
 # Creating a dashboard header
-st.header('Patient Follow-Up')
+st.header('Patient Follow-Up & Vehicle Accident Data')
 
 # Creating a caption
 st.caption('Patient follow-up recorded within the last year')
@@ -14,6 +15,9 @@ st.caption('Patient follow-up recorded within the last year')
 if st.checkbox('Hospital 03 Follow-Up'):
     st.dataframe(registry_df)
 
+if st.checkbox('Vehicle Accident Data'):
+    st.dataframe(vehicle_df)
+
 # Creating a barchart
 st.subheader('Display of Patients Primary Hospital')
 hosp_num = registry_df['Hospital_Code'].value_counts()
@@ -21,9 +25,14 @@ st.bar_chart(hosp_num)
 st.caption('Visual provides insight of the number of patients per hospital')
 
 # Creating a line chart
-st.subheader('Displaying the Date of Last Contact')
-last_contact = registry_df['Last_Contact']
-st.line_chart(last_contact)
-st.caption('Visual provides insight of the date of last contact')
+st.subheader('Accidents based on the Day of the Week')
+day_week = vehicle_df['Day_of_Week'].value_counts()
+st.bar_chart(day_week)
+st.caption('Visual provides insight of the number of patients per hospital')
 
 # Creating a code block
+code = '''## Code behind the Bar Chart for Day of the Week
+day_week = vehicle_df['Day_of_Week'].value_counts()
+st.bar_chart(day_week)'''
+st.code(code, language='python')
+
